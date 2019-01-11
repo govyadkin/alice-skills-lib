59a07c3aeffbc042548d5dcd3d67f367a740d224
govyadkin <43575482+govyadkin@users.noreply.github.com>
2019-01-11 16:01:05 +0300

    Update Structs.hpp

--- includes/Structs.hpp
+++ includes/Structs.hpp
@@ -20,3 +20,24 @@ struct ForwardList2 {
     };
     Node* Head;
 };
+
+ForwardList polsk(const std::vector<std::string>& v, Alice::Response& response);
+double trigon(size_t& i, const std::vector<std::string>& v, Alice::Response& response);
+double calc(ForwardList& list);
+std::vector<std::string> split(const std::string& str);
+
+double Strtodouble(const std::string str);
+
+void Construct(ForwardList& list);
+void Destruct(ForwardList& list);
+void PushFront(ForwardList& list, std::string value);
+std::string PopFront(ForwardList& list);
+size_t Size(const ForwardList& list);
+void Reverse(ForwardList& list);
+
+void Construct(ForwardList2& list);
+void Destruct(ForwardList2& list);
+void PushFront(ForwardList2& list, double value);
+double PopFront(ForwardList2& list);
+size_t Size(const ForwardList2& list);
+void MyCallback(const Alice::Request& request, Alice::Response& response);
